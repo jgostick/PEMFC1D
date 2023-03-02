65c8b9438ee11ac450172d186857358d726fd36b
Jeff Gostick <jgostick@gmail.com>
2023-03-02 10:44:00 -0500

    more or less working, some numerical stability issues

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ import openpnm as op
 import matplotlib.pyplot as plt
 from copy import copy
 from scipy import constants as c
+import numpy as np
 
 
 state = st.session_state
 if not hasattr(state, 'CO2cat'):
     state.CO2cat = 1.0
 
-tabs = st.tabs(["Conditions", "Dimensions"])
+
+tabs = st.tabs(["Channel", "GDL", "Membrane", "Catalyst"])
 
 with tabs[0]:
-    cols0 = st.columns(2)
-    cols0[0].markdown("**Cathode**")
-    Tcat = cols0[0].number_input(
+    cols1 = st.columns(2)
+    cols1[0].markdown("### Input data")
+    Tcat = cols1[0].number_input(
         label='Temperture [K]',
         key='T_cathode',
         value=350,
@@ -22,15 +24,15 @@ with tabs[0]:
         min_value=275,
         max_value=372,
     )
-    Pcat = cols0[0].number_input(
+    Pcat = cols1[0].number_input(
         label='Pressure [kPa]',
         key='P_cathode',
         value=150,
         step=10,
         min_value=100,
         max_value=500,
-    )
-    RHcat = cols0[0].number_input(
+    )*1000  # Convert to Pa
+    RHcat = cols1[0].number_input(
         label='Relative Humidity [%]',
         key='RH_cathode',
         value=80,
@@ -38,113 +40,182 @@ with tabs[0]:
         min_value=0,
         max_value=100,
     )
-    xO2cat = 0.21*(1 - RHcat/100)
-    Ccat = 101.323*Pcat/(c.R*Tcat)
-    CO2cat = Ccat*xO2cat
-
-    cols0[1].markdown("**Anode**")
-    Tan = cols0[1].number_input(
-        label='Temperture [K]',
-        key='T_anode',
-        value=Tcat,
-        step=5,
-        min_value=275,
-        max_value=372,
+    cols1[1].markdown("### Conditions")
+    A = 8.07131
+    B = 1730.63
+    C = 233.426
+    PV = 10**(A-B/(C+Tcat-273))/760*101325
+    cols1[1].text_input(
+        label='Vapor Pressure [Pa]',
+        value=str(np.around(PV, decimals=0)),
         disabled=True,
     )
-    Pan = cols0[1].number_input(
-        label='Pressure [kPa]',
-        key='P_anode',
-        value=150,
-        step=10,
-        min_value=101,
-        max_value=300,
+    Cgas = Pcat/(c.R*Tcat)
+    cols1[1].text_input(
+        label='Molar Density [mol/m3]',
+        value=str(np.around(Cgas, decimals=0)),
+        disabled=True,
     )
-    RHan = cols0[1].number_input(
-        label='Relative Humidity [%]',
-        key='RH_anode',
-        value=80,
-        step=5,
-        min_value=0,
-        max_value=100,
+    xO2cat = 0.21*(1 - RHcat/100*PV/(Pcat))
+    cols1[1].text_input(
+        label='Oxygen mol fraction',
+        value=str(np.around(xO2cat, decimals=4)),
+        disabled=True,
     )
+    CO2cat = Cgas*xO2cat
+    cols1[1].text_input(
+        label='Oxygen concentration [mol/m3]',
+        value=str(np.around(CO2cat, decimals=0)),
+        disabled=True,
+    )
+
 
 with tabs[1]:
     cols1 = st.columns(2)
-    L = cols1[0].number_input(
+    L_GDL = cols1[0].number_input(
         label='GDL Thickness [um]',
         value=300,
         step=5,
         min_value=50,
         max_value=500,
-    )
+    )*1e-6  # Convert to m
     e = cols1[0].number_input(
         label='GDL Porosity [%]',
         value=75,
         step=5,
         min_value=10,
         max_value=90,
-    )
-    A = cols1[0].number_input(
-        label='Anisotropy Ratio [%]',
-        help='The ratio of in-plane to through-plane transport rate',
-        value=1.0,
+    )/100  # Convert to fraction
+    a = cols1[0].slider(
+        label='Bruggeman Exponent',
+        value=2.0,
         step=0.1,
-        min_value=0.01,
+        min_value=0.0,
         max_value=10.0,
     )
-    taux = (e/100)**-2
-    tauy = taux/A
-    cols1[0].text_input(
+    taux = (e)**-a
+    cols1[1].text_input(
         label='Tortuosity',
-        value=str(taux) + ' | ' + str(tauy),
+        value=str(np.around(taux, decimals=3)),
+        disabled=True,
+    )
+    DAB = 0.0000209/(298.0**1.5)*(Tcat**1.5)
+    cols1[1].text_input(
+        label='Gas Diffusivity [m2/s]',
+        value=str(np.around(DAB, decimals=8)),
+        disabled=True,
+    )
+    ilim = 4*96487*e/taux*DAB/L_GDL*(CO2cat-0)/10000
+    cols1[1].text_input(
+        label='Limiting current [A/cm2]',
+        value=str(np.around(ilim, decimals=3)),
         disabled=True,
     )
 
+with tabs[2]:
+    cols1 = st.columns(2)
+    L_PEM = cols1[0].number_input(
+        label='PEM Thickness [um]',
+        value=300,
+        step=5,
+        min_value=10,
+        max_value=500,
+    )*1e-6
+    sigma = cols1[0].slider(
+        label='Ionic Conductivity',
+        value=10.0,
+        step=0.1,
+        min_value=0.0010,
+        max_value=100.0,
+    )
+
+with tabs[3]:
+    cols1 = st.columns(2)
+    io = cols1[0].number_input(
+        label='Exchange Current Density [A/m2]',
+        value=1.0,
+        step=0.1,
+        min_value=1.0e-5,
+        max_value=1.0e5,
+        format="%e",
+    )
+    alpha = cols1[0].slider(
+        label='Transfer coefficient',
+        value=0.5,
+        step=0.05,
+        min_value=0.0,
+        max_value=1.0,
+    )
+    z = cols1[1].text_input(
+        label='Electrons per mol',
+        value=str(4),
+        disabled=True,
+    )
+
+st.markdown("---")
+tabs = st.tabs(["Polarization Curve", "Voltage Profiles"])
+
+
 # %%
-state.shape = [20, 100]
-if st.button(label='Run Simulation'):
-    pn = op.network.Cubic(state.shape, spacing=1)
-    pn.add_model(propname='throat.vector',
-                 model=op.models.geometry.throat_vector.pore_to_pore)
-    air = op.phase.Air(network=pn)
-    air['pore.temperature'] = Tcat
-    air['pore.pressure'] = Pcat
-    air.regenerate_models()
-    Tsx = pn['throat.vector'][:, 0] == 1
-    air['throat.diffusive_conductance'] = 0.0
-    air['throat.diffusive_conductance'][Tsx] = air['throat.diffusivity'][Tsx]*e/taux
-    Tsy = pn['throat.vector'][:, 1] == 1
-    air['throat.diffusive_conductance'][Tsy] = air['throat.diffusivity'][Tsy]*e/tauy
+def find_eta(E_cell, eta=None):
+    if eta is None:
+        eta = E_cell - 1.22
+    # Find slope of error at current guess
+    eta_calc = []
+    eta_guess = []
+    eta_guess.append(eta*0.99)
+    eta_calc.append(calc_eta(E_cell=E_cell, eta_guess=eta_guess[0]))
+    eta_guess.append(eta*1.01)
+    eta_calc.append(calc_eta(E_cell=E_cell, eta_guess=eta_guess[0]))
+
+    for i in range(1, 10):
+        # Use Newton's method to guess next eta
+        err_1 = eta_guess[i-1] - eta_calc[i-1]
+        err_2 = eta_guess[i] - eta_calc[i]
+        m = (err_2 - err_1)/(eta_guess[i] - eta_guess[i-1])
+        eta_guess.append((0 - err_2)/m + eta_guess[i])
+        eta_calc.append(calc_eta(E_cell=E_cell, eta_guess=eta_guess[i]))
+        # if m**2 < 1e-10:
+            # break
+
+    return eta_calc[-1]
 
 
-    m = op.models.physics.source_terms.standard_kinetics
-    air.add_model(propname='pore.reaction',
-                  model=m,
-                  X='pore.concentration',
-                  prefactor=-0.01,
-                  exponent=1,
-                  regen_mode='deferred')
-
-    fd = op.algorithms.FickianDiffusion(network=pn, phase=air)
-    Ps = pn['pore.right']*(pn.coords[:, 1] < 80)*(pn.coords[:, 1] > 20)
-    fd.set_value_BC(pores=Ps, values=CO2cat)
-    fd.set_source(pores=pn['pore.left'], propname='pore.reaction')
-    fd.run()
-    state.conc = fd['pore.concentration']
-    state.CO2cat = CO2cat
+def calc_eta(E_cell, eta_guess):
+    i = calc_i(E_cell=E_cell, eta=eta_guess)
+    phi_e = E_cell
+    phi_H_calc = 0 - i*(L_PEM)/sigma
+    eta_calc = phi_e - phi_H_calc - 1.22
+    return eta_calc
 
 
+def calc_i(E_cell, eta):
+    k = io*np.exp(-4*96487*(1-alpha)*eta/(8.314*Tcat))
+    g = 4*96487*DAB*(e/taux)/(L_GDL)
+    CO2_CL = g*CO2cat/(g + k)
+    i = k*CO2_CL
+    # i = g*(CO2cat - CO2_CL)
+    return i
 
-if hasattr(state, 'conc'):
-    fig, ax = plt.subplots()
-    ax.axis(False)
-    im = ax.imshow(state.conc.reshape(state.shape),
-                   interpolation='bilinear',
-                   cmap=plt.cm.plasma,
-                   vmin=0, vmax=state.CO2cat)
-    fig.colorbar(im)
-    st.pyplot(fig)
 
+E_cell = np.arange(1.2, 0.0, -0.02)
+i_cell = []
+for E in E_cell:
+    eta = find_eta(E_cell=E)
+    i = calc_i(E_cell=E, eta=eta)
+    i_cell.append(i)
+
+fig, ax = plt.subplots()
+ax.plot(i_cell, E_cell, 'b-o')
+tabs[0].pyplot(fig)
+
+with tabs[1]:
+    E = st.slider(
+        label='Cell Voltage',
+        value=1.0,
+        step=0.05,
+        min_value=0.1,
+        max_value=1.2,
+    )
 
 
